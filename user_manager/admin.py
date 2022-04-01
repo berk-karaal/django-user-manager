d04be1a461afed30ec35fd53a865a26b9aba6fba
Berk Karaal <karaalberk7@gmail.com>
2022-04-01 19:55:04 +0300

    register User model to the admin site

--- user_manager/admin.py
+++ user_manager/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = ("date_joined",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+
+    list_display = ("email", "is_staff")
+    search_fields = ("email",)
+    ordering = ("email",)
