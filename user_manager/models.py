f14abff8707d4c9dc290f70bcacfc275ad429b0d
Berk Karaal <karaalberk7@gmail.com>
2022-04-01 19:43:11 +0300

    add a user manager and connect it to the User model

--- user_manager/models.py
+++ user_manager/models.py
@@ -1,6 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 from django.conf import settings
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with given email and password
+        """
+        if not email:
+            raise ValueError("Users must have an email address.")
+        if not password:
+            raise ValueError("Users must have a password.")
+
+        user = self.model(email=self.normalize_email(email=email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a Superuser with given email and password
+        """
+        user = self.create_user(email=email, password=password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -11,6 +42,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_email_verified = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
 
     def __str__(self) -> str:
         return self.email
